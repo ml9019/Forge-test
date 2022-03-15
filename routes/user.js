e85ed03bcda0bfad929382d4c46d0ff6bc1bcb6b
Mateusz Lukasiewicz <Mateusz.Lukasiewicz@keoic.com>
2022-03-15 13:10:56 +0400

    A

--- routes/user.js
+++ routes/user.js
@@ -29,25 +29,22 @@ router.get("/user/profile", async (req, res) => {
   const user = new UserProfileApi();
   const profile = await user.getUserProfile(oauth.getClient(), internalToken);
 
-  
-    //ML:
-    let em = profile.body.emailId
-    var val = "";
-    if (em.indexOf("@keoic.com") > 0) {
-        val = "KEO user";
-    }
-    else {
-        val = "Other user";
-    }
-    //
-    
-    res.json({
-        name: profile.body.firstName + ' ' + profile.body.lastName,
-        picture: profile.body.profileImages.sizeX40,
-        email: profile.body.emailId,
-        userName: profile.body.userName,
-        isKEOuser: val
+  //ML:
+  let em = profile.body.emailId;
+  var val = "";
+  if (em.indexOf("@keoic.com") > 0) {
+    val = "KEO user";
+  } else {
+    val = "Other user";
+  }
+  //
 
-    });
-
+  res.json({
+    name: profile.body.firstName + " " + profile.body.lastName,
+    picture: profile.body.profileImages.sizeX40,
+    email: profile.body.emailId,
+    userName: profile.body.userName,
+    isKEOuser: val,
+  });
+});
 module.exports = router;
