9eb3acc94298e9f4e19a0f9d7b0eccbb6ee04e65
Mateusz Lukasiewicz <Mateusz.Lukasiewicz@keoic.com>
2022-03-15 12:51:52 +0400

    A

--- routes/user.js
+++ routes/user.js
@@ -16,22 +16,22 @@
 // UNINTERRUPTED OR ERROR FREE.
 /////////////////////////////////////////////////////////////////////
 
-const express = require('express');
-const { UserProfileApi } = require('forge-apis');
+const express = require("express");
+const { UserProfileApi } = require("forge-apis");
 
-const { OAuth } = require('./common/oauth');
+const { OAuth } = require("./common/oauth");
 
 let router = express.Router();
 
-router.get('/user/profile', async (req, res) => {
-    const oauth = new OAuth(req.session);
-    const internalToken = await oauth.getInternalToken();
-    const user = new UserProfileApi();
-    const profile = await user.getUserProfile(oauth.getClient(), internalToken);
-    res.json({
-        name: profile.body.firstName + ' ' + profile.body.lastName,
-        picture: profile.body.profileImages.sizeX40
-    });
+router.get("/user/profile", async (req, res) => {
+  const oauth = new OAuth(req.session);
+  const internalToken = await oauth.getInternalToken();
+  const user = new UserProfileApi();
+  const profile = await user.getUserProfile(oauth.getClient(), internalToken);
+  res.json({
+    name: profile.body.firstName + " " + profile.body.lastName,
+    picture: profile.body.profileImages.sizeX100,
+  });
 });
 
 module.exports = router;
