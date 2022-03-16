7ddea5ecb35088f2a09e9a879f568a307dbf0aae
Mateusz Lukasiewicz <Mateusz.Lukasiewicz@keoic.com>
2022-03-16 10:07:15 +0400

    forge update

--- routes/datamanagement.js
+++ routes/datamanagement.js
@@ -214,7 +214,7 @@ async function getVersions(projectId, itemId, oauthClient, credentials, res) {
       return createTreeNode(
         viewerUrn,
         decodeURI(
-          version.attributes.modelGuid +
+          version.data.modelGuid +
             "-" +
             "v" +
             versionst +
