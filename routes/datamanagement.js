41f43d0dac87d67ddcd292539dafa5805f0df21c
Mateusz Lukasiewicz <Mateusz.Lukasiewicz@keoic.com>
2022-03-16 12:16:12 +0400

    forge update

--- routes/datamanagement.js
+++ routes/datamanagement.js
@@ -178,6 +178,12 @@ async function getFolderContents(
   );
   const treeNodes = contents.body.data.map((item) => {
     //const test = contents.body.included[0].attributes.name || "1";
+
+    const test2 = JSON.stringify(contents);
+
+    // Covert JSON string into object
+    const bookObject = JSON.parse(test2);
+
     const test = "1"; //      contents.body.included[0].attributes.extension.data.modelGuid || "1";
     var name =
       item.attributes.name == null
